38dc7dd517fd29b8ca557a81d42e33ef9355f0d2
littletof <szalay.kristof.sopron@gmail.com>
2020-07-31 18:01:17 +0200

    example

--- benchmarks/pr_benchmarks.ts
+++ benchmarks/pr_benchmarks.ts
@@ -5,12 +5,15 @@ import {
   thresholdResultColumn,
   thresholdsColumn,
   extraMetricsColumns,
+  GroupDefinition,
+  ColumnDefinition,
 } from "https://deno.land/x/pretty_benching@feature%2Fbenchmark_down/pretty_benchmark_down.ts";
 
 import {
   runBenchmarks,
   bench,
   BenchmarkResult,
+  BenchmarkRunResult,
 } from "https://deno.land/std@0.62.0/testing/bench.ts";
 
 import * as colors from "https://deno.land/std@0.62.0/fmt/colors.ts";
@@ -119,6 +122,8 @@ bench({
 const thresholds = {
   "Rotating arrays": { green: 2.5, yellow: 3.4 },
   "Sorting arrays": { green: 0.5, yellow: 2 },
+  "Proving NP==P": { green: 4141, yellow: 4600},
+  "Standing out": { green: 0.300, yellow: 0.330 }
 };
 
 const indicators = [
@@ -133,7 +138,8 @@ const indicators = [
 
 runBenchmarks(
   { silent: true, skip: /_long/ },
-).then(prettyBenchmarkDown(
+)
+/* .then(prettyBenchmarkDown(
   console.log,
   {
     title: "An example benchMarkdown",
@@ -196,4 +202,70 @@ runBenchmarks(
       },
     ],
   },
+));*/
+.then(prettyBenchmarkDown(
+  console.log,
+  {
+    title: "An example benchMarkdown",
+    description: (runResult: BenchmarkRunResult) => `This markdown was generated with the use of \`prettyBenchmarkDown\`, the only things changed, are the code blocks, which were inserted afterwards.\nIf you use a function for the \`description\`, you can process the results here as well: \n\n > In this benchmark ${runResult.results.length} benches were run, ${runResult.filtered} were filtered.`,
+    afterTables: "\n---\n\nThis behaves the same as \`description\`, it just puts this at the bottom of the markdown.\nHere its defined with a simple string",
+    groups: [
+      {
+        include: /array/,
+        name: "Default columns and dynamic text",
+        description: "This is a group's \`description\`.\nHere you can see what the default columns are, and how you can use a `function` as `description` or `afterTable` inside a group",
+        afterTable: (gr: BenchmarkResult[], g: GroupDefinition, rr: BenchmarkRunResult) => `This is a group's \`afterTable\`.\nHere you can access eg. the group name: \`${g.name}\`, benchmarks in this group: \`${gr.length}\` of them here, or the whole \`BenchmarkRunResult\`: \`${rr.results.length}\` benchmarks total`,
+        columns: [...defaultColumns]
+      },
+      {
+        include: /array/,
+        name: "Custom columns",
+        afterTable: "If you see `-`, that means the value there was `undefined`, if you see `*` it means that column is badly configured, no `formatter` or `propertyKey` was defined.",
+        columns: [
+          ...defaultColumns,
+          {
+            title: 'CustomTotal',
+            propertyKey: 'totalMs',
+            toFixed: 5,
+            align: 'left'
+          },
+          {
+            title: 'Formatter',
+            formatter: (r: BenchmarkResult, cd: ColumnDefinition) => `${r.name.split("").reverse().join("")}:${cd.title.split("").reverse().join("")} <-`
+          },
+          {
+            title: 'Undefined',
+            propertyKey: 'dontHaveOneLikeThis'
+          },
+          {
+            title: 'Bad Config'
+          }
+        ]
+      },
+      {
+        include: /otating|Proving|Standing/,
+        name: "Predefiend columns",
+        description: "Here you can see, what the predefined columns are.\n\nYou can add the `indicators` and `thresholds` that you use in `prettyBenchmarkProgress` and `prettyBenchmarkResults`.\n\nYou can see, how you can rename columns like with `Thresholds+`",
+        columns: [
+          indicatorColumn(indicators),
+          ...defaultColumns,
+          thresholdsColumn(thresholds),
+          {...thresholdsColumn(thresholds, true), title: 'Thresholds+'},
+          thresholdResultColumn(thresholds),
+        ],
+      },
+      {
+        include: /./,
+        name: "Extra metrics",
+        description: "You can add `extraMetrics` columns too. If you set `ignoreSingleRuns` to `true`, it wont show values on rows, where runCount is 1.",
+        columns: [
+          ...defaultColumns,
+          ...extraMetricsColumns({ ignoreSingleRuns: true }),
+        ],
+      },
+    ]
+  },
 ));
+//.then(prettyBenchmarkDown(console.log))
+
+// \n\`\`\`ts\n THE WHOLE GENERATOR CODE \n\`\`\`\n
