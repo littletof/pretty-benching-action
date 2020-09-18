a5e9017173c1dfc6c8c9bf58f3ba323f099007ed
littletof <szalay.kristof.sopron@gmail.com>
2020-09-18 14:47:44 +0200

    test CI-s

--- benchmarks/pr_benchmarks.ts
+++ benchmarks/pr_benchmarks.ts
@@ -7,16 +7,20 @@ import {
   extraMetricsColumns,
   GroupDefinition,
   ColumnDefinition,
-} from "https://deno.land/x/pretty_benching@v0.2.1/pretty_benchmark_down.ts";
+} from "https://deno.land/x/pretty_benching@v0.3.0/pretty_benchmark_down.ts";
+
+import {historyColumns, deltaColumn} from 'https://deno.land/x/pretty_benching@v0.3.0/history_extensions.ts';
+import {prettyBenchmarkHistory} from 'https://deno.land/x/pretty_benching@v0.3.0/pretty_benchmark_history.ts';
 
 import {
   runBenchmarks,
   bench,
   BenchmarkResult,
   BenchmarkRunResult,
-} from "https://deno.land/std@0.62.0/testing/bench.ts";
+} from "https://deno.land/std@0.69.0/testing/bench.ts";
 
-import * as colors from "https://deno.land/std@0.62.0/fmt/colors.ts";
+import * as colors from "https://deno.land/std@0.69.0/fmt/colors.ts";
+import type { BenchIndicator, Thresholds } from "https://deno.land/x/pretty_benching@v0.3.0/types.ts";
 
 bench({
   name: "Sorting arrays",
@@ -119,88 +123,138 @@ bench({
   },
 });
 
-const thresholds = {
+const thresholds: Thresholds = {
   "Rotating arrays": { green: 3.5, yellow: 4.4 },
   "Sorting arrays": { green: 0.5, yellow: 2 },
-  "Proving NP==P": { green: 4141, yellow: 6000},
-  "Standing out": { green: 0.300, yellow: 0.330 }
+  "Proving NP==P": { green: 4141, yellow: 6000 },
+  "Standing out": { green: 0.300, yellow: 0.330 },
 };
 
-const indicators = [
+const indicators: BenchIndicator[] = [
   { benches: /NP/, modFn: () => colors.magenta("%") },
   { benches: /array/, modFn: () => "🎹" },
   {
     benches: /Standing/,
     modFn: () => "🚀",
-    tableColor: colors.magenta,
+    color: colors.magenta,
   },
 ];
+
+console.log(Deno.args);
+
+let historicData;
+try {
+  historicData = JSON.parse(
+    Deno.readTextFileSync("./benchmarks/benchmark_history.json"),
+  );
+} catch (e) {
+  console.warn(`⚠ cant read history file. (${e.message})`);
+}
+const history = new prettyBenchmarkHistory(historicData, {strict: true, runExtras: ()=> Deno.version});
+
+let markdownString: string;
 
 runBenchmarks(
   { silent: true, skip: /_long/ },
 )
-.then(prettyBenchmarkDown(
-  console.log,
-  {
-    title: "An example benchMarkdown",
-    description: (runResult: BenchmarkRunResult) => `This markdown was generated with the use of \`prettyBenchmarkDown\`, the only things changed, are the code blocks, which were inserted afterwards.\nIf you use a function for the \`description\` or \`afterTables\`, you can process the results here as well: \n\n > In this benchmark ${runResult.results.length} benches were run, ${runResult.filtered} were filtered.`,
-    afterTables: "\n---\n\nThis is the `afterTables`. This behaves the same as \`description\`, it just puts this at the bottom of the markdown.\nHere its defined with a simple string.\n\nCheck out the Github Action, which comments a markdown like this on PRs: $link",
-    groups: [
-      {
-        include: /array/,
-        name: "Default columns and dynamic text",
-        description: "This is a group's \`description\`.\nHere you can see what the default columns are, and how you can use a `function` as `description` or `afterTable` inside a group",
-        afterTable: (gr: BenchmarkResult[], g: GroupDefinition, rr: BenchmarkRunResult) => `This is a group's \`afterTable\`.\nHere you can access eg. the group name: \`${g.name}\`, benchmarks in this group: \`${gr.length}\` of them here, or the whole \`BenchmarkRunResult\`: \`${rr.results.length}\` benchmarks total`,
-        columns: [...defaultColumns]
-      },
-      {
-        include: /array/,
-        name: "Custom columns",
-        afterTable: "If you see `-`, that means the value there was `undefined`, if you see `*` it means that column is badly configured, no `formatter` or `propertyKey` was defined.",
-        columns: [
-          defaultColumns[0],
-          defaultColumns[1],
-          defaultColumns[2],
-          {
-            title: 'CustomTotal',
-            propertyKey: 'totalMs',
-            toFixed: 5,
-            align: 'left'
-          },
-          {
-            title: 'Formatter',
-            formatter: (r: BenchmarkResult, cd: ColumnDefinition) => `${r.name.split("").reverse().join("")}:${cd.title.split("").reverse().join("")} <-`
-          },
-          {
-            title: 'Undefined',
-            propertyKey: 'dontHaveOneLikeThis'
-          },
-          {
-            title: 'Bad Config'
-          }
-        ]
-      },
-      {
-        include: /otating|Proving|Standing/,
-        name: "Predefiend columns",
-        description: "Here you can see, what the predefined columns are.\n\nYou can add the `indicators` and `thresholds` that you use in `prettyBenchmarkProgress` and `prettyBenchmarkResults`.\n\nYou can see, how you can rename columns like with `Thresholds+`",
-        columns: [
-          indicatorColumn(indicators),
-          ...defaultColumns,
-          thresholdsColumn(thresholds),
-          {...thresholdsColumn(thresholds, true), title: 'Thresholds+'},
-          thresholdResultColumn(thresholds),
-        ],
-      },
-      {
-        include: /i/,
-        name: "Extra metrics",
-        description: "You can add `extraMetrics` columns too. In its `metrics` array you can define which columns you want. If you set `ignoreSingleRuns` to `true`, it wont show values on rows, where runCount is 1.",
-        columns: [
-          ...defaultColumns,
-          ...extraMetricsColumns({ ignoreSingleRuns: true }),
-        ],
-      },
-    ]
-  },
-));
+  .then(prettyBenchmarkDown(
+    (markdown) => { markdownString = markdown },
+    {
+      title: "An example benchMarkdown",
+      description: (runResult: BenchmarkRunResult) =>
+        `This markdown was generated with the use of \`prettyBenchmarkDown\`.\nIf you use a function for the \`description\` or \`afterTables\`, you can process the results here as well: \n\n > In this benchmark ${runResult.results.length} benches were run, ${runResult.filtered} were filtered.`,
+      afterTables:
+        "\n---\n\nThis is the `afterTables`. This behaves the same as \`description\`, it just puts this at the bottom of the markdown.\nHere its defined with a simple string.\n\nCheck out the Github Action, which comments a markdown like this on PRs: $link",
+      groups: [
+        {
+          include: /array/,
+          name: "Default columns and dynamic text",
+          description:
+            "This is a group's \`description\`.\nHere you can see what the default columns are, and how you can use a `function` as `description` or `afterTable` inside a group",
+          afterTable: (
+            gr: BenchmarkResult[],
+            g: GroupDefinition,
+            rr: BenchmarkRunResult,
+          ) =>
+            `This is a group's \`afterTable\`.\nHere you can access eg. the group name: \`${g.name}\`, benchmarks in this group: \`${gr.length}\` of them here, or the whole \`BenchmarkRunResult\`: \`${rr.results.length}\` benchmarks total`,
+          columns: [...defaultColumns()],
+        },
+        {
+          include: /array/,
+          name: "Custom columns",
+          afterTable:
+            "If you see `-`, that means the value there was `undefined`, if you see `*` it means that column is badly configured, no `formatter` or `propertyKey` was defined.",
+          columns: [
+            ...defaultColumns(["name", "runsCount", "totalMs"]),
+            {
+              title: "CustomTotal",
+              propertyKey: "totalMs",
+              toFixed: 5,
+              align: "left",
+            },
+            {
+              title: "Formatter",
+              formatter: (r: BenchmarkResult, cd: ColumnDefinition) =>
+                `${r.name.split("").reverse().join("")}:${
+                  cd.title.split("").reverse().join("")
+                } <-`,
+            },
+            {
+              title: "Undefined",
+              propertyKey: "dontHaveOneLikeThis",
+            },
+            {
+              title: "Bad Config",
+            },
+          ],
+        },
+        {
+          include: /otating|Proving|Standing/,
+          name: "Predefiend columns",
+          description:
+            "Here you can see, what the predefined columns are.\n\nYou can add the `indicators` and `thresholds` that you use in `prettyBenchmarkProgress` and `prettyBenchmarkResults`.\n\nYou can see, how you can rename columns like with `Thresholds+`",
+          columns: [
+            indicatorColumn(indicators),
+            ...defaultColumns(),
+            thresholdsColumn(thresholds),
+            { ...thresholdsColumn(thresholds, true), title: "Thresholds+" },
+            thresholdResultColumn(thresholds),
+          ],
+        },
+        {
+          include: /i/,
+          name: "Extra metrics",
+          description:
+            "You can add `extraMetrics` columns too. In its `metrics` array you can define which columns you want. If you set `ignoreSingleRuns` to `true`, it wont show values on rows, where runCount is 1.",
+          columns: [
+            ...defaultColumns(),
+            ...extraMetricsColumns({ ignoreSingleRuns: true }),
+          ],
+        },
+        {
+          include: /i/,
+          name: "Tracking history",
+          description:
+            "There are also predefined columns for you to use with `prettyBenchmarkHistory`. There is `historyColumns`, which gives you each historic run's result as a separate column, and `deltaColumn`, which shows you how much the results changed from the previous run for each benchmark.",
+          columns: [
+            ...defaultColumns(["name", "runsCount"]),
+            ...historyColumns(history),
+            {title: "Current", align: "right", propertyKey: "measuredRunsAvgMs", toFixed: 4},
+            deltaColumn(history)
+          ],
+        },
+      ],
+    },
+  )).then(results => {
+    
+    if(Deno.args.indexOf("--save-history") !== -1) {
+      const id = Deno.args.find(a => a.startsWith("--id="))?.replace("--id=", "");
+      console.log(`Saving history${id ? ` with id: ${id}`: ""}`);
+      Deno.writeTextFileSync("./benchmarks/benchmark_history.json", history.addResults(results, {id}).getDataString());
+    }
+    if(Deno.args.indexOf("--save-markdown") !== -1) {
+      console.log("Saving markdown");
+      Deno.writeTextFileSync("./benchmarks/pr_benchmark.md", markdownString);
+    }
+
+  });
